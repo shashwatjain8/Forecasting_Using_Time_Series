6129629f29ae5561dafa43d5b7983a544e393233
Shashwat Jain <shashwat.jain@wdc.com>
2018-11-24 19:57:38 +0530

    ARIMA Prediction

--- predict.py
+++ predict.py
@@ -1,6 +1,7 @@
 import warnings
 import itertools
 import numpy as np
+from pylab import rcParams
 import matplotlib.pyplot as plt
 warnings.filterwarnings("ignore")
 plt.style.use('fivethirtyeight')
@@ -11,3 +12,75 @@ matplotlib.rcParams['axes.labelsize'] = 14
 matplotlib.rcParams['xtick.labelsize'] = 12
 matplotlib.rcParams['ytick.labelsize'] = 12
 matplotlib.rcParams['text.color'] = 'k'
+print "asd"
+df = pd.read_excel("dataset.xls")
+furniture = df.loc[df['Category'] == 'Furniture']
+furniture['Order Date'].min(), furniture['Order Date'].max()
+
+cols = ['Row ID', 'Order ID', 'Ship Date', 'Ship Mode', 'Customer ID', 'Customer Name', 'Segment', 'Country', 'City', 'State', 'Postal Code', 'Region', 'Product ID', 'Category', 'Sub-Category', 'Product Name', 'Quantity', 'Discount', 'Profit']
+furniture.drop(cols, axis=1, inplace=True)
+furniture = furniture.sort_values('Order Date')
+print furniture.isnull().sum()
+
+#print furniture[:3]
+
+furniture = furniture.groupby('Order Date')
+furniture = furniture['Sales'].sum().reset_index()
+
+#print furniture.head()
+#print furniture[:3]
+furniture = furniture.set_index('Order Date')
+print furniture.index
+y = furniture['Sales'].resample('MS').mean()
+
+print y['2017':]
+
+# y.plot(figsize=(15, 6))
+# plt.show()
+
+# rcParams['figure.figsize'] = 18, 8
+# decomposition = sm.tsa.seasonal_decompose(y, model='additive')
+# fig = decomposition.plot()
+# plt.show()
+
+
+#ARIMA
+
+p = d = q = range(0, 2)
+pdq = list(itertools.product(p, d, q))
+seasonal_pdq = [(x[0], x[1], x[2], 12) for x in list(itertools.product(p, d, q))]
+print('Examples of parameter combinations for Seasonal ARIMA...')
+print('SARIMAX: {} x {}'.format(pdq[1], seasonal_pdq[1]))
+print('SARIMAX: {} x {}'.format(pdq[1], seasonal_pdq[2]))
+print('SARIMAX: {} x {}'.format(pdq[2], seasonal_pdq[3]))
+print('SARIMAX: {} x {}'.format(pdq[2], seasonal_pdq[4]))
+
+for param in pdq:
+    for param_seasonal in seasonal_pdq:
+        try:
+            mod = sm.tsa.statespace.SARIMAX(y,
+                                            order=param,
+                                            seasonal_order=param_seasonal,
+                                            enforce_stationarity=False,
+                                            enforce_invertibility=False)
+
+            results = mod.fit()
+
+            print('ARIMA{}x{}12 - AIC:{}'.format(param, param_seasonal, results.aic))
+        except:
+            continue
+
+
+mod = sm.tsa.statespace.SARIMAX(y,
+                                order=(1, 1, 1),
+                                seasonal_order=(1, 1, 0, 12),
+                                enforce_stationarity=False,
+                                enforce_invertibility=False)
+
+results = mod.fit()
+
+print(results.summary().tables[1])
+
+
+results.plot_diagnostics(figsize=(16, 8))
+plt.show()
