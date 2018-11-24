02b10af605912a3b74aa7a16dbbcb656c9fb3412
Shashwat Jain <shashwat.jain@wdc.com>
2018-11-24 18:15:05 +0530

    Prediction Script

---
+++ predict.py
@@ -0,0 +1,13 @@
+import warnings
+import itertools
+import numpy as np
+import matplotlib.pyplot as plt
+warnings.filterwarnings("ignore")
+plt.style.use('fivethirtyeight')
+import pandas as pd
+import statsmodels.api as sm
+import matplotlib
+matplotlib.rcParams['axes.labelsize'] = 14
+matplotlib.rcParams['xtick.labelsize'] = 12
+matplotlib.rcParams['ytick.labelsize'] = 12
+matplotlib.rcParams['text.color'] = 'k'
